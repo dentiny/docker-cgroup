8b8c44adc4638a2f3d31efd877dc1ca7ade225bb
dentiny <dentinyhao@gmail.com>
2025-03-01 01:14:18 +0000

    Add mmap script

---
+++ mmap_allocation.cc
@@ -0,0 +1,24 @@
+// This script keeps allocation memory via `mmap` to check whether memory constraint applies for mmap.
+
+#include <cassert>
+#include <cerrno>
+#include <chrono>
+#include <cstring>
+#include <cstdlib>
+#include <thread>
+
+#include <unistd.h>
+#include <sys/mman.h>
+
+int main() {
+  const size_t page_size = sysconf(_SC_PAGESIZE);
+  
+  while (true) {
+    void* addr = mmap(nullptr, page_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    assert(addr != MAP_FAILED);      
+    memset(addr, 0, page_size);
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+  }
+  
+  return EXIT_SUCCESS;
+}
